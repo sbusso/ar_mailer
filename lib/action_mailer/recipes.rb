5fd261a60134b379efc593fa4a63128d49c310e5
Stephane Busso <stephane.busso@gmail.com>
2010-05-09 16:41:03 +0200

    give capistrano recipes to manage daemon

---
+++ lib/action_mailer/recipes.rb
@@ -0,0 +1,21 @@
+# Capistrano Recipes for managing ar_mailer
+#
+#
+#   after "deploy:stop",    "delayed_job:stop"
+#   after "deploy:start",   "delayed_job:start"
+
+
+Capistrano::Configuration.instance.load do
+  namespace :ar_mailer do
+    desc "Stop the ar_mailer process"
+    task :stop, :roles => :app do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} rake sendmail:stop"
+    end
+
+    desc "Start the ar_mailer process"
+    task :start, :roles => :app do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} rake sendmail:star"
+    end
+
+  end
+end
